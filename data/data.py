cd09f496074727e3c42f2694a7f22fb329e85c77
Mykhailo Babych <93287705+plxgxx@users.noreply.github.com>
2023-01-04 04:57:40 +0200

    Add files via upload

--- data/data.py
+++ data/data.py
@@ -1,1 +1,17 @@
+import json
+import os
+import re
 
+import pytz
+
+directory_path = os.path.dirname(os.path.abspath(os.path.abspath(__file__)))
+new_path = os.path.join(directory_path, "text.json")
+
+with open(new_path, "r", encoding="utf-8") as fp:
+    text = json.load(fp)
+
+
+TIMER_RANGE = [6, 10]
+
+kyiv_tz = os.getenv("TIME_ZONE", "Europe/Kiev")
+TIME_ZONE = pytz.timezone(kyiv_tz)
